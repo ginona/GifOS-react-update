7db25873ce0aae26e3558a09c9599d8b46ef8d52
ginona <ginonacchio94@gmail.com>
2025-06-05 11:30:09 -0300

    Simplify configuration

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,32 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  return {
-    plugins: [react()],
-    server: {
-      port: 3000,
-      open: true,
-      proxy: {
-        '/api/giphy': {
-          target: 'https://api.giphy.com/v1',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api\/giphy/, ''),
-          configure: (proxy, _options) => {
-            proxy.on('proxyReq', (proxyReq, req, _res) => {
-              // Add API key to all requests
-              const url = new URL(proxyReq.path, 'https://api.giphy.com');
-              url.searchParams.append('api_key', env.VITE_GIPHY_API_KEY);
-              proxyReq.path = url.pathname + url.search;
-            });
-          },
-        }
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api/giphy': {
+        target: 'https://api.giphy.com/v1',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/giphy/, ''),
+        configure: (proxy, _options) => {
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            const url = new URL(proxyReq.path, 'https://api.giphy.com');
+            url.searchParams.append('api_key', process.env.VITE_GIPHY_API_KEY);
+            proxyReq.path = url.pathname + url.search;
+          });
+        },
       }
-    },
-    build: {
-      outDir: 'build',
-      sourcemap: true
-    },
-    define: {
-      'import.meta.env.VITE_GIPHY_API_KEY': JSON.stringify(env.VITE_GIPHY_API_KEY)
     }
+  },
+  build: {
+    outDir: 'build',
+    sourcemap: true
+  },
+  define: {
+    'import.meta.env.VITE_GIPHY_API_KEY': JSON.stringify(process.env.VITE_GIPHY_API_KEY)
   }
 }) 
